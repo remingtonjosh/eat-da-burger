0326407ead4a411e1219517460a1035b6a41b15a
Remington Villa <remingtonvilla@gmail.com>
2019-11-06 13:39:08 -0600

    jaws

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,13 +1,10 @@
-$(document).ready(function () {
-
-    $(".eatburger").on("click", function (event) {
-        var burgerid = $(this).attr("data-id");
-        console.log(burgerid);
-        $.ajax({
-            method: "PUT",
-            URL: "/api/burgers/" + burgerid
-        }).then(function (data) {
-            location.reload();
-        })
-    });
+$(document).on("click", ".eatburger", function(event) {
+  var burgerid = $(this).attr("data-id");
+  var query = "/api/burgers/" + burgerid;
+  $.ajax({
+    method: "PUT",
+    URL: query
+  }).then(function(data) {
+    location.reload();
+  });
 });
